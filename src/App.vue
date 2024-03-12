b3c51dc851a0707f7a026b52ac16cbcd8962a5d8
陈龙 <chen_teacher@163.com>
2024-03-12 23:57:04 +0800

    init: 初始化项目

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import CanInputDiv from "./components/canInputDiv.vue";
+</script>
+
+<template>
+  <CanInputDiv/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/canInputDiv.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+import {defineProps, CSSProperties} from 'vue';
+
+interface CanInputDivProps {
+  placeholder?: string
+  placeholderStyle?: CSSProperties
+}
+withDefaults(defineProps<CanInputDivProps>(), {
+  placeholder: '测试文字内容123',
+  placeholderStyle: {
+    color: '#999',
+    fontSize: '1em'
+  }
+})
+</script>
+
+<template>
+  <div class="can-input-div" :style="$attrs.style" :class="$attrs.class">
+    <span class="placeholder" :style="placeholderStyle">{{ placeholder }}</span>
+    <div contenteditable="true" class="input-div">
+      <span style="background: #f00;" contenteditable="false">测试文字</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.can-input-div{
+  font-size: 20px;
+  background-color: #efefef;
+  position: relative;
+  padding: 10px;
+}
+.can-input-div > *{
+  font-size: 1em;
+  position: relative;
+  z-index: 1;
+}
+.input-div{
+  caret-color: #00f;
+  outline: none;
+}
+.placeholder{
+  position: absolute;
+  z-index: 0;
+  width: 100%;
+  pointer-events: none;
+}
+</style>
